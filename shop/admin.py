12b934ecd46408c3db58880cb2b3ff21450d74f8
onium16 <onium16@gmail.com>
2023-10-27 12:18:19 +0200

    site without mutilang and pay service

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
 
 # Register your models here.
 from . import models
 
 admin.site.register(models.Category)
 admin.site.register(models.Product)
+
+class CustomAdminSite(AdminSite):
+    site_header = 'Admin Panel Onicorp Official site'
+    site_title = 'Admin Panel Onicorp'
