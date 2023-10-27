3fe6862d66fa1b0ad97c46ee48519c83ed2adeed
onium16 <onium16@gmail.com>
2023-10-27 18:02:06 +0200

    correct titles admin panel

--- base/settings.py
+++ base/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+from django.utils.translation import gettext_lazy as _
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
     'shop.apps.ShopConfig',
 ]
 
@@ -126,6 +127,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Media files
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
--- shop/admin.py
+++ shop/admin.py
@@ -7,6 +7,12 @@ from . import models
 admin.site.register(models.Category)
 admin.site.register(models.Product)
 
+
 class CustomAdminSite(AdminSite):
-    site_header = 'Admin Panel Onicorp Official site'
-    site_title = 'Admin Panel Onicorp'
+
+    # print(AdminSite.site_header)
+    AdminSite.site_header = "Admin Panel Onicorp Official site"
+    AdminSite.site_title = 'Admin Panel Onicorp'
+    AdminSite.index_title = ''
+
+
